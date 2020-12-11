cfd536e1867dcdac8a99aec4d2782675739d8b6e
r87977ph <peter.hamer@student.manchester.ac.uk>
2020-12-11 08:59:52 +0000

    Initialising database

---
+++ dbTest.php
@@ -0,0 +1,68 @@
+<?php 
+    $testMsgs = true; // true = On, false = Off.
+
+    //Create connection
+    $conn = mysqli_connect($database_host, $database_user, $database_pass, $group_dbnames[0]);
+
+    //Check connection
+    if (!$conn){
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    echo("Connected successfully");
+
+    $sql = "CREATE DATABASE frigoDB";
+    doSQL($conn, $sql, $testMsgs);
+
+    $sql = "
+    CREATE TABLE user (
+        user_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        forename VARCHAR(30) NOT NULL,
+        surname VARCHAR(30) NOT NULL,
+        email VARCHAR(50) NOT NULL UNIQUE,
+        password VARCHAR(128) NOT NULL
+    )
+    ";
+    doSQL($conn, $sql, $testMsgs);
+
+    $sql = "
+    CREATE TABLE ing (
+        ing_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        ingredient VARCHAR(30) NOT NULL
+    )
+    ";
+    doSQL($conn, $sql, $testMsgs);
+
+    $sql = "
+    CREATE TABLE user_ing (
+        user_id INT(6) NOT NULL,
+        ing_id INT(6) NOT NULL,
+        PRIMARY KEY (user_id, ing_id)
+    )
+    ";
+    doSQL($conn, $sql, $testMsgs);
+
+    $sql = "
+    CREATE TABLE user_rec (
+        user_id INT(6) NOT NULL,
+        rec_id INT(6) NOT NULL,
+        date_added DATE NOT NULL,
+        PRIMARY KEY (user_id, rec_id)
+    )
+    ";
+    doSQL($conn, $sql, $testMsgs);
+
+
+    function doSQL($conn, $sql, $testMsgs){
+        if ($testMsgs){
+            echo("<br><code>SQL: $sql</code>");
+            if ($result = $conn->query($sql)){
+                echo("<code> - OK</code>");
+            } else{
+                echo("<code> - FAIL! " . $conn->error . " </code>");
+            }
+        } else{
+            $result = $conn->query($sql);
+        }
+        return $result;
+    }
+?>
