cafb698bb8f326f96274e0b7edd3478d04646c89
r87977ph <peter.hamer@student.manchester.ac.uk>
2020-12-11 12:06:42 +0000

    basic structure

---
+++ home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Register</title>
+        <link rel="stylesheet" href="register.css">
+    </head>
+    <body>
+        <header id="headerwrap">
+            <h1>FRIGO</h1>
+        </header>
+        <nav>
+            <a href="">Find a recipe</a> |
+            <a href="myAccount.php">My Account</a> |
+            <a href="">Website Settings</a>
+        </nav>
+    </body>
+</html>
