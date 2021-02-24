8cc23d280acdde361933b9eb0ef0234a47827f74
r87977ph <peter.hamer@student.manchester.ac.uk>
2021-02-24 14:21:29 +0000

    Trying to gather recipe data and order it

--- testAPI.php
+++ testAPI.php
@@ -4,55 +4,89 @@
         <title>Testing API</title>
     </head>
     <body>
-    <?php
-		$para = $_POST['searchPara'];	    
-    	$formattedpara = str_replace(", ", "%2C+", $para);
-    	#echo $formattedpara;
-    	$url = "https://recipe-puppy.p.rapidapi.com/?i=" . $formattedpara ;
-    	echo($url . "<br>");
+    	<?php
+    	#Gather recipe IDs matching ingredietns from list
+    	$array = array("Eggs", "Chicken", "Cheese", "Oil", "water");
+    	$rec = array();
+    	foreach ($array as $ing){
+    		$curl = curl_init();
 
-		$curl = curl_init();
+			  curl_setopt_array($curl, [
+			    CURLOPT_URL => "https://www.themealdb.com/api/json/v2/9973533/filter.php?i=" . $ing,
+			    CURLOPT_RETURNTRANSFER => true,
+			    CURLOPT_FOLLOWLOCATION => true,
+			    CURLOPT_ENCODING => "",
+			    CURLOPT_MAXREDIRS => 10,
+			    CURLOPT_TIMEOUT => 30,
+			    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			    CURLOPT_CUSTOMREQUEST => "GET",
+			  ]);
 
-		curl_setopt_array($curl, [
-			CURLOPT_URL => 	$url,
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_FOLLOWLOCATION => true,
-			CURLOPT_ENCODING => "",
-			CURLOPT_MAXREDIRS => 10,
-			CURLOPT_TIMEOUT => 30,
-			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-			CURLOPT_CUSTOMREQUEST => "GET",
-			CURLOPT_HTTPHEADER => [
-				"x-rapidapi-host: recipe-puppy.p.rapidapi.com",
-				"x-rapidapi-key: 6009943639msh16b18fc4b42ba8ep1a7df9jsn55a697cdbcec"
-			],
-		]);
+			  $response = curl_exec($curl);
+			  $err = curl_error($curl);
 
-		$response = curl_exec($curl);
-		$err = curl_error($curl);
+			  curl_close($curl);
 
-		curl_close($curl);
+			  if ($err) {
+			    die ("cURL Error #:" . $err);
+			  } else {
+			    #echo $response;
+			  }
+			  $response = json_decode($response, true);
+		  	  $response = $response['meals'];
+		  	  foreach ($response as $meal){
+		  	  	array_push($rec, $meal['idMeal']);
+		  	  }
+    	}
 
-		if ($err) {
-			die("cURL Error #:" . $err);
-		} else {
-			echo $response;
-		}
+    	#An attempt of trying to count number of matching ingredients for each recipe
+    	foreach ($rec as $id){
+    		echo $id . "<br>";
+    		$curl = curl_init();
 
-		$response = json_decode($response, true);
-		$response = $response['results'];
+			  curl_setopt_array($curl, [
+			    CURLOPT_URL => "https://www.themealdb.com/api/json/v2/9973533\lookup.php?i=" . $id,
+			    CURLOPT_RETURNTRANSFER => true,
+			    CURLOPT_FOLLOWLOCATION => true,
+			    CURLOPT_ENCODING => "",
+			    CURLOPT_MAXREDIRS => 10,
+			    CURLOPT_TIMEOUT => 30,
+			    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			    CURLOPT_CUSTOMREQUEST => "GET",
+			  ]);
 
-		foreach ($response as $item){
-			echo("Recipe: " . $item['title'] . "<br>");
-			echo("<a href='" . $item['href'] . "'>View Here</a><br><br>");
-		}
+			  $response = curl_exec($curl);
+			  $err = curl_error($curl);
 
-		echo("<form method='POST'>");
-		echo("<input type='text'  name='searchPara' id='searchPara'>");
-		echo("<input type='submit' value='search'>");
-		echo("</form>");				
-				
+			  curl_close($curl);
 
-    ?>
+			  if ($err) {
+			    die ("cURL Error #:" . $err);
+			  } else {
+			    #echo $response;
+			  }
+
+			  
+			  $response = json_decode($response, true);
+		  	  $response = $response['meals'];
+		  	  $number = array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+		  	 $count = 0;
+		  	 $countarray = array(5);
+		  	 foreach($response as $meal){
+		  	  foreach ($number as $num){
+		  	  	foreach($array as $ing){
+		  	  		if ($meal['strIngredient' . $num] == $ing){
+		  	  			$count = $count + 1;
+		  	  		}
+		  	  	}
+		  	  }
+		  	  array_push($countarray, $count);
+		  	}
+		  	echo($count . "<br>");
+    	}
+    	foreach($countarray as $x){
+    		echo $x;
+    	}
+		?>
     </body>
 </html>
