ce18863a9435c4b99d8b8edeecaff9024f8ee7b2
u71590jn <jiayi.nie@student.manchester.ac.uk>
2021-02-27 22:16:33 +0800

    react version framework 1st

---
+++ z9-groupproject-react/src/view/GenerateRecipes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '.././index.css';
+import { Layout, Menu,Breadcrumb} from 'antd';
+
+export default class Recipes extends React.Component {
+
+  render() {
+    return (
+        <div className="site-layout-background" style={{ padding: 10, minHeight: 300 }}>
+          <Breadcrumb style={{ margin: '10px 0' }}>
+                  <Breadcrumb.Item style={{color:"#fa8c16"}}>FRIGO</Breadcrumb.Item>
+                  <Breadcrumb.Item style={{color:"#fa8c16"}}>Customize Recipes</Breadcrumb.Item>
+                  <Breadcrumb.Item style={{color:"#fa8c16"}}>Generated Recipes</Breadcrumb.Item>
+          </Breadcrumb>
+          <p>Recipes generating...</p>
+        </div>
+       
+    );
+  }
+}
